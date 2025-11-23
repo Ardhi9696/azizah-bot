ce404f482eb9a3cc1c6894f36676ecb680e5487b
Ardhi9696 <ardhihyunjae96@gmail.com>
2025-11-23 19:00:01 +0700

    perubahan

--- start_monitor_root.sh
+++ start_monitor_root.sh
@@ -1,5 +1,3 @@
 #!/system/bin/sh
-
-# Jalankan Flask dalam tmux sebagai root
 su -c "/data/data/com.termux/files/usr/bin/tmux new-session -d -s monitor \
 'cd /data/data/com.termux/files/home/Azizah-Bot && /data/data/com.termux/files/usr/bin/python3 monitor_flask.py'"
